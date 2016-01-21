8b5f4c01f563be4a842e9ca1f05095eb7634541d
Michael Ceraso <mmc7709@michaels-mbp.home>
2016-01-21 01:12:50 -0500

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Michael Ceraso on 1/21/16.
+//  Copyright © 2016 Michael Ceraso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
